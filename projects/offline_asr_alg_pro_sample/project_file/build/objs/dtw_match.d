d7098ced50dcf79c2a44c94bdf967f0b4b672030
lordyoung <421800172@qq.com>
2026-05-30 22:03:20 +0800

    1.26

---
+++ projects/offline_asr_alg_pro_sample/project_file/build/objs/dtw_match.d
@@ -0,0 +1,7 @@
+build/objs/dtw_match.o: ../../../components/mfcc_dtw_spk/dtw_match.c \
+ ../../../components/mfcc_dtw_spk/dtw_match.h \
+ ../../../projects/offline_asr_alg_pro_sample/app/app_main/user_config.h
+
+../../../components/mfcc_dtw_spk/dtw_match.h:
+
+../../../projects/offline_asr_alg_pro_sample/app/app_main/user_config.h:
---
+++ projects/offline_asr_alg_pro_sample/project_file/build/objs/flacdecoder.d
@@ -0,0 +1,14 @@
+build/objs/flacdecoder.o: \
+ ../../../components/player/flacdec/flacdecoder.c \
+ ../../../projects/offline_asr_alg_pro_sample/app/app_main/user_config.h \
+ ../../../components/player/flacdec/golomb.h \
+ ../../../components/player/flacdec/bitstreamf.h \
+ ../../../components/player/flacdec/flacdecoder.h
+
+../../../projects/offline_asr_alg_pro_sample/app/app_main/user_config.h:
+
+../../../components/player/flacdec/golomb.h:
+
+../../../components/player/flacdec/bitstreamf.h:
+
+../../../components/player/flacdec/flacdecoder.h:
