dd13ab27d5b97a628526958b2fda2adba4b6701f
lordyoung <421800172@qq.com>
2026-06-09 13:31:26 +0800

    1.47

--- projects/offline_asr_alg_pro_sample/project_file/build/objs/simple_audio_player.d
+++
@@ -1,5 +0,0 @@
-build/objs/simple_audio_player.o: \
- ../../../components/simple_audio_player/simple_audio_player.c \
- ../../../projects/offline_asr_alg_pro_sample/app/app_main/user_config.h
-
-../../../projects/offline_asr_alg_pro_sample/app/app_main/user_config.h:
