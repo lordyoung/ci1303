28f7ce326a050f77de72a838d3e494a97e516ff9
lordyoung <421800172@qq.com>
2026-06-09 21:26:52 +0800

    1.52

--- projects/offline_asr_alg_pro_sample/app/app_main/servo.h
+++ projects/offline_asr_alg_pro_sample/app/app_main/servo.h
@@ -9,8 +9,8 @@
 #define SERVO_PWM_CH        PWM2            /* 舵机 PWM 通道 */
 #define SERVO_PWM_PAD       PA4             /* 舵机信号脚 = PA4 */
 #define SERVO_PWM_PAD_FUNC  FIFTH_FUNCTION  /* PA4 第5功能=PWM2 (查ci130x_scu.h引脚表) */
-#define SERVO_ANGLE_A       90              /* "小屁锁车" 角度 */
-#define SERVO_ANGLE_B       0               /* "小屁开门" 通过后角度 */
+#define SERVO_ANGLE_A       0              /* "小屁锁车" 角度 */
+#define SERVO_ANGLE_B       90               /* "小屁开门" 通过后角度 */
 
 #define SERVO_CMD_OPEN_DOOR 1
 #define SERVO_CMD_LOCK_CAR  5
